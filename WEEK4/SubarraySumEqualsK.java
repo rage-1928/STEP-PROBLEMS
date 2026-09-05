5e60609cd6f54222eee25ff0f6365ba4c40b5be7
rage-1928 <aarohan192813@gmail.com>
2026-09-05 21:30:30 +0530

    Add week 4 assignments

---
+++ WEEK4/SubarraySumEqualsK.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SubarraySumEqualsK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            currentSum = currentSum + nums[i];
+
+            int required = currentSum - k;
+
+            if (map.containsKey(required)) {
+                count = count + map.get(required);
+            }
+
+            if (map.containsKey(currentSum)) {
+                map.put(currentSum, map.get(currentSum) + 1);
+            } else {
+                map.put(currentSum, 1);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
