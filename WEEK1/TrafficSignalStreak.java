a39b564363a6c59c68b43bbb241191a3501e0ee8
rage-1928 <aarohan192813@gmail.com>
2026-09-05 20:58:56 +0530

    Add week 1 assignments

---
+++ WEEK1/TrafficSignalStreak.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class TrafficSignalStreak {
+
+    static void findLongestStreak(String signalLog) {
+
+        if (signalLog.length() == 0) {
+
+            System.out.println("No Signal Data");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int longest = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int current = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == currentColor) {
+                current++;
+            } else {
+                currentColor = signalLog.charAt(i);
+                current = 1;
+            }
+
+            if (current > longest) {
+                longest = current;
+                longestColor = currentColor;
+            }
+        }
+
+        System.out.println(
+            "Longest Streak: '" +
+            longestColor +
+            "' repeated " +
+            longest +
+            " times"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+
+        findLongestStreak(signalLog);
+    }
+}
+    
